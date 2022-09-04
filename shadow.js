861d8730c992966133995aa88b3ad65d51b3f94f
chobomc <112730784+chobomc@users.noreply.github.com>
2022-09-05 02:53:30 +0900

    111

---
+++ shadow.js
@@ -0,0 +1,8 @@
+const body = document.querySelector(".flip");
+const Fcard = document.querySelector("#front");
+const Bcard = document.querySelector("#back");
+const button = document.querySelector(".pressMe");
+const title = document.querySelector("#title");
+
+window.addEventListener("click", togglebutton);
+// Bcard.addEventListener('click', togglebutton)
